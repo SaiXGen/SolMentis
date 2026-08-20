766cf36329b052fced669f99b687878fb62aaf90
Saikat Mitra <smitra0135@gmail.com>
2026-08-21 02:25:18 +0530

    Connect frontend to online backend

--- frontend/app.js
+++ frontend/app.js
@@ -1471,7 +1471,7 @@ async function analyze() {
     const response =
       await fetch(
 
-        "http://127.0.0.1:8000/analyze",
+        "https://solmentis.onrender.com/analyze",
 
         {
 
